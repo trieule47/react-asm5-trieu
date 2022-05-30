7636a2585db0fa2753c415ed715dbaf91483e9fc
vinhtrieulnvt <vinhtrieulnvt@gmail.com>
2022-05-30 16:08:34 +0700

    update signup

--- src/containers/LoginView.js
+++ src/containers/LoginView.js
@@ -1,12 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../App.css';
 
 export default function LoginView() {
     const [username, SetUsername] = useState('');
     const [pass, SetPass] = useState('');
-
+    const [users, setUsers] = useState('');
     let navigate = useNavigate();
+
+    const getUsers = () => {
+        setUsers(JSON.stringify(localStorage.getItem('users')));
+        console.log(users);
+    }
     const handleUsernameChange = (e) => {
         SetUsername(e.target.value)
     }
@@ -15,9 +20,10 @@ export default function LoginView() {
         SetPass(e.target.value)
     }
 
-    const hanldeLogin = () => {
-        console.log('handle login :' + username+ 'da');
-        if(username === 'admin' && pass === 'admin') {
+    const handleLogin = () => {
+        getUsers();
+        console.log('handle login :' + username + 'da');
+        if (username === 'admin' && pass === 'admin') {
             localStorage.setItem('user', '1');
             navigate('/');
         }
@@ -25,9 +31,12 @@ export default function LoginView() {
 
     return (
         <div className='App'>
-            <h3>Username</h3><input type="text" onChange={(e) => handleUsernameChange(e)} />
-            <h3>Password</h3><input type="text" onChange={(e) => handlePassChange(e)} />
-            <h3><button onClick={() => hanldeLogin()}>Login</button></h3>
+            <div className='form-input'>
+                <h2>Login</h2>
+                <h3>Username</h3><input type="text" onChange={(e) => handleUsernameChange(e)} />
+                <h3>Password</h3><input type="text" onChange={(e) => handlePassChange(e)} />
+                <h3><input type='submit' value='Login' onClick={() => handleLogin()} /></h3>
+            </div>
         </div>
     )
 }
--- src/containers/SignUp.js
+++ src/containers/SignUp.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
 
 import '../App.css';
 
@@ -9,31 +10,39 @@ export default function SignUp() {
   const { register, handleSubmit, formState: { errors } } = useForm();
   const [data, setData] = useState("");
   const [lists, setLists] = useState([]);
+  const navigate = useNavigate();
   const onSubmit = data => console.log(data);
 
   const handleAccoutAdd = (data) => {
     setData(JSON.stringify(data));
-    setLists([...lists, data ]);
-    localStorage.setItem('users',JSON.stringify(lists));
+    console.log(data);
+    setLists([...lists, data]);
+    console.log(lists);
+    localStorage.setItem('users',JSON.parse(lists));
     const List = JSON.parse(localStorage.getItem('users'));
     console.log(List);
+    navigate('/');
   }
 
   return (
     <div className='App'>
-      <form onSubmit={handleSubmit((data) => handleAccoutAdd(data))} >
-        <input {...register("UserName", { pattern: /^(?=.*[a-z])[A-Za-z\d]{3,15}$/ })} placeholder="User name" />
-        {errors?.Username?.type === "pattern" && (
-          <p>UserName Minimum 3 characters and maximum 15 characters, at least one lowercase letter,</p>
-        )}
-        <br></br><input {...register("Password", { pattern: /^(?=.*[a-z])(?=.*\d)[A-Za-z\d]{3,15}$/ })} placeholder="Password" />
-        {errors?.Password?.type === "pattern" && (
-          <p>Password Minimum 3 characters and maximum 15 characters, at least one lowercase letter, one number</p>
-        )}
-        <br></br>
-        <input type="submit" />
+      <div className='form-input'>
+        <h2>Sign Up</h2>
+        <form onSubmit={handleSubmit((data) => handleAccoutAdd(data))} >
+          <input {...register("UserName", { pattern: /^(?=.*[a-z])[A-Za-z\d]{3,15}$/ })} placeholder="User name" />
+          {errors?.UserName?.type === "pattern" && (
+            <p>Password Minimum 3 characters and maximum 15 characters, at least one lowercase letter</p>
+          )}
+          <br></br><input {...register("Password", { pattern: /^(?=.*[a-z])(?=.*\d)[A-Za-z\d]{3,15}$/ })} placeholder="Password" />
+          {errors?.Password?.type === "pattern" && (
+            <p>Password Minimum 3 characters and maximum 15 characters, at least one lowercase letter, one number</p>
+          )}
+          <br></br>
+          <input type="submit" />
+        </form>
         <p>{data}</p>
-      </form>
+        <input onClick={()=> navigate('/')} type="submit" value="login" />
+      </div>
     </div>
   )
 }
