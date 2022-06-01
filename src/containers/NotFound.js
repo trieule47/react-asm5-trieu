2a3ecf1104732958483e6ceb0ad642a715577dd2
vinhtrieulnvt <vinhtrieulnvt@gmail.com>
2022-06-01 16:34:36 +0700

    update

---
+++ src/containers/NotFound.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function NotFound() {
+    return (
+        <div style={{ margin: 'auto', padding: 'auto', fontSize: 30, color: 'black'}}>
+            NotFound
+            <p>đường dẫn không tồn tại</p>
+        </div>
+    )
+}
